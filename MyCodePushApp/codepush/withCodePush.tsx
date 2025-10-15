fa0b3e2da29f3e6e2d1a1ff8c50bca5177b36352
shubhamguptadream11 <shubham.gupta@dream11.com>
2025-10-15 16:08:31 +0530

    feat: added events for client side sample app

--- MyCodePushApp/codepush/withCodePush.tsx
+++ MyCodePushApp/codepush/withCodePush.tsx
@@ -27,60 +27,60 @@ export const withCodePush = (config: Partial<CodePushConfig> = {}) => {
       // Use only above mentioned statuses
       switch (status) {
         case codePush.SyncStatus.UP_TO_DATE:
-          console.log('Codepush Event: Up to date');
+          console.log('Codepush Client Event: Up to date');
           break;
         case codePush.SyncStatus.DOWNLOADING_PACKAGE:
-          console.log('Codepush Event: Downloading package');
+          console.log('Codepush Client Event: Downloading package');
           break;
         case codePush.SyncStatus.INSTALLING_UPDATE:
-          console.log('Codepush Event: Installing update');
+          console.log('Codepush Client Event: Installing update');
           break;
         case codePush.SyncStatus.UPDATE_INSTALLED:
-          console.log('Codepush Event: Update installed');
+          console.log('Codepush Client Event: Update installed');
           break;
         case codePush.SyncStatus.UPDATE_IGNORED:
-          console.log('Codepush Event: Update ignored');
+          console.log('Codepush Client Event: Update ignored');
           break;
         case codePush.SyncStatus.UNKNOWN_ERROR:
-          console.log('Codepush Event: Unknown error');
+          console.log('Codepush Client Event: Unknown error');
             break;
         case codePush.SyncStatus.SYNC_IN_PROGRESS:
-          console.log('Codepush Event: Sync in progress');
+          console.log('Codepush Client Event: Sync in progress');
           break;
         case codePush.SyncStatus.CHECKING_FOR_UPDATE:
-          console.log('Codepush Event: Checking for update');
+          console.log('Codepush Client Event: Checking for update');
           break;
         case codePush.SyncStatus.AWAITING_USER_ACTION:
-          console.log('Codepush Event: Awaiting user action');
+          console.log('Codepush Client Event: Awaiting user action');
           break;
         case codePush.SyncStatus.PATCH_APPLIED_SUCCESS:
-          console.log('Codepush Event: Patch applied success');
+          console.log('Codepush Client Event: Patch applied success');
           break;
         case codePush.SyncStatus.DOWNLOAD_REQUEST_SUCCESS:
-          console.log('Codepush Event: Download request success');
+          console.log('Codepush Client Event: Download request success');
           break;
         case codePush.SyncStatus.UNZIPPED_SUCCESS:
-          console.log('Codepush Event: Unzipped success');
+          console.log('Codepush Client Event: Unzipped success');
           break;
         case codePush.SyncStatus.UPDATE_AVAILABLE:
-          console.log('Codepush Event: Update available');
+          console.log('Codepush Client Event: Update available');
           break;
         case codePush.SyncStatus.UPDATE_IGNORED_ROLLBACK:
-          console.log('Codepush Event: Update available ignored rollback');
+          console.log('Codepush Client Event: Update available ignored rollback');
           break;
         case codePush.SyncStatus.DECOMPRESSED_SUCCESS:
-          console.log('Codepush Event: Decompressed success');
+          console.log('Codepush Client Event: Decompressed success');
           break;
         default:
-          console.log('Codepush Event: Unknown status', status);
+          console.log('Codepush Client Event: Unknown status', status);
           break;
       }
       }, (downloadProgress) => {
-      console.log('Codepush Event: Download progress', downloadProgress);
+      console.log('Codepush Client Event: Download progress', downloadProgress);
     }).then((update) => {
-      console.log('Codepush Event: Then Debugging', update);
+      console.log('Codepush Client Event: Then Debugging', update);
     }).catch((error) => {
-      console.log('Codepush Event: Catch debugging', error.message);
+      console.log('Codepush Client Event: Catch debugging', error.message);
     });
 
     return codePush(defaultConfig)(WrappedComponent);
