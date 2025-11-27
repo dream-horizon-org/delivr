3a98df4c83b28d60f2cba78166f955a2d065551d
Devansh Saini <devansh.saini@dream11.com>
2025-11-27 16:57:49 +0530

    fix: dummy commit

--- script/command-executor.ts
+++ script/command-executor.ts
@@ -1628,6 +1628,7 @@ function getSdk(accessKey: string, headers: Headers, customServerUrl: string): A
    * to delete the cached connection so the user can simply
    * login again instead of having to log out first.
    */
+  
   Object.getOwnPropertyNames(AccountManager.prototype).forEach((functionName: any) => {
     if (typeof sdk[functionName] === "function") {
       const originalFunction = sdk[functionName];
