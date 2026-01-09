98f91bf4850f334f852b8b9c631b1cdd033f9085
Thotaharshavardhani <harshavardhani.thota@dream11.com>
2026-01-09 16:32:47 +0530

    fix: navigation to releases from distributions

--- app/components/Pages/components/Sidebar/Module.tsx
+++ app/components/Pages/components/Sidebar/Module.tsx
@@ -43,8 +43,7 @@ export function Module({
           location.pathname.includes("/releases") &&
           !location.pathname.includes("/releases/configurations") &&
           !location.pathname.includes("/releases/configure") &&
-          !location.pathname.includes("/releases/workflows") &&
-          location.pathname !== `/dashboard/${org.id}/releases`
+          !location.pathname.includes("/releases/workflows") 
         ) {
           return true;
         }
