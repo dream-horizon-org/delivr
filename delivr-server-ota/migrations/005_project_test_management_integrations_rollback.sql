149d178d78955e9eec09efd7bb11396189033594
prathameshy7 <prathamesh.yadav@dream11.com>
2026-01-21 12:54:25 +0530

    chore: removed unnecessary files from delivr-server-ota

--- delivr-server-ota/migrations/005_project_test_management_integrations_rollback.sql
+++
@@ -1,4 +0,0 @@
--- Rollback Migration: Drop tenant_test_management_integrations table
--- Date: 2024-11-20
-
-DROP TABLE IF EXISTS tenant_test_management_integrations;
--- delivr-server-ota/migrations/007_test_management_configs_rollback.sql
+++
@@ -1,4 +0,0 @@
--- Rollback Migration: Drop test_management_configs table
--- Date: 2024-11-20
-
-DROP TABLE IF EXISTS test_management_configs;
